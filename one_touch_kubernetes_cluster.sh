e011921ef5bb19de5f54cb9521f7eb2bfb754c3c
caisarus <cezar.alexandru.vasilescu@gmail.com>
2017-11-26 21:26:43 +0000

    Ansible cluster set up

---
+++ one_touch_kubernetes_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Prepare hosts for ansible
+#Ensure hosts match to inventory file
+#Ensure hosts file is updated on control machine
+#You will be asked first time for the password for the SSH connection. We recomend to use same password for all hosts
+echo "Preparing ansible master"
+ansible-playbook playbooks/prepare-ansible-master.yml
+
+echo "Please enter SSH password for the servers"
+ansible-playbook playbooks/prepare-ansible-target.yml -k
+
+echo "Starting kubernetes deployment"
+ansible-playbook kubernetes.yml
+
+echo "After running this script you will need to run only "ansible-playbook kubernetes.yml" as the cluster is already set up"
